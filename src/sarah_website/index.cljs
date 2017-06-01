330726ea72c7a2361d60cb2cc3e4566ff704aeb5
sarnegard <sarah.arnegard@gmail.com>
2017-06-01 12:19:12 -0500

    chunked more-projects, nav, and heading into functions

--- src/sarah_website/index.cljs
+++ src/sarah_website/index.cljs
@@ -1,5 +1,21 @@
 (ns sarah-website.index)
 
+(defn nav []
+  [:div.container-lrg
+   [:div.col-12.spread
+    [:div [:a.logo "Sarah Arnegard"]]
+    [:div
+     [:a.nav-link {:href "#work"} "Work "]
+     [:a.nav-link {:href "https://blog.saraharnegard.com"} "Blog"]
+     [:a.nav-link
+      {:href "/resume.html"}
+      "Resume"]]]])
+(defn about-me []
+  [:div.container-sml.text-center
+   [:div.col-12
+    [:h1.heading
+     "What drives me: I’m driven by discovering insights that lead to meaningful improvements in people’s lives"]]])
+
 (defn more-projects [{:keys [project-title project-desc project-link]}]
   [:div
     [:h4 project-title]
@@ -38,37 +54,27 @@
       {:rel "stylesheet", :type "text/css", :href "css/devices.css"}]]
     [:body
      [:header.header
-      [:div.container-lrg
-       [:div.col-12.spread
-        [:div [:a.logo "Sarah Arnegard"]]
-        [:div
-         [:a.nav-link {:href "#"} "Work "]
-         [:a.nav-link {:href "#"} "Blog"]
-         [:a.nav-link
-          {:href "#"}
-          "Resume"]]]]
-      [:div.container-sml.text-center
-       [:div.col-12
-        [:h1.heading
-         "What drives me:\n            I’m driven by discovering insights that lead to meaningful improvements in people’s lives\n          "]]]]
+      [nav []]
+      [about-me[]]]
+
      [:div.feature3
       [:div.container-lrg.flex
-       "\n        What I specialize in:\n        "
+       "What I specialize in:  "
        [:div.col-4
         [:h3.subheading
-         "\n            Research and experiment design\n          "]
+         "    Research and experiment design    "]
         [:p.paragraph
-         "\n            Whether it's discovering pains or testing product market fit, I prefer to test hypotheses and examine evidence rather than argue about assumptions.\n          "]]
+         "      Whether it's discovering pains or testing product market fit, I prefer to test hypotheses and examine evidence rather than argue about assumptions.    "]]
        [:div.col-4
         [:h3.subheading
-         "\n            Product and service design\n          "]
+         "      Product and service design    "]
         [:p.paragraph
-         "\n            Rapid ideation, prototyping, and testing that involves the team in the process of reaching a solution that delights our customers.\n          "]]
+         "      Rapid ideation, prototyping, and testing that involves the team in the process of reaching a solution that delights our customers.    "]]
        [:div.col-4
         [:h3.subheading
-         "\n            Collaboration and facilitation\n          "]
+         "      Collaboration and facilitation    "]
         [:p.paragraph
-         "\n            I'm passionate about bringing the team on the journey from empathy to solution, rather than work in a silo. It's my mission to improve the user centered design skills.\n          "]]]]
+         "      I'm passionate about bringing the team on the journey from empathy to solution, rather than work in a silo. It's my mission to improve the user centered design skills.    "]]]]
      [:div.feature4
       [:div.container-lrg [:h2 "What I've worked on"]]
       [:div.container-lrg.flex
@@ -76,37 +82,37 @@
         [:div.androidiphone.sidedevices
          [:div.phones
           [:img {:src "img/phone_tyro.png", :alt ""}]
-          "<!-- <div class=\"iphone\">\n                <div class=\"mask\">\n                  <img class=\"mask-img\" src=\"img/ios_app.png\">\n                </div>\n              </div>\n              <div class=\"android\">\n                <div class=\"mask\">\n                  <img class=\"mask-img\" src=\"img/android_app.png\">\n                </div>\n              </div> -->"]]]
+          "<!-- <div class=\"iphone\">          <div class=\"mask\">            <img class=\"mask-img\" src=\"img/ios_app.png\">          </div>        </div>        <div class=\"android\">          <div class=\"mask\">            <img class=\"mask-img\" src=\"img/android_app.png\">          </div>        </div> -->"]]]
        [:div.col-1]
        [:div.col-5.centervertical
-        [:h3.subheading "\n            Tyro Payments\n          "]
+        [:h3.subheading "      Tyro Payments    "]
         [:p.paragraph
-         "\n            I was hired as a generalist Product Designer to work on "
+         "      I was hired as a generalist Product Designer to work on "
          [:a
           {:href "https://tyro.com/business-loans/"}
           "Tyro Smart Growth Funding"]
-         ", the first business lending product that allows customers to accept loans on a mobile platform, eliminating the need for paper applications and phone calls.\n          "]
+         ", the first business lending product that allows customers to accept loans on a mobile platform, eliminating the need for paper applications and phone calls.    "]
         [:p.paragraph
-         "\n            Later on I specialized in Customer Research, working on discovery research, experiment design, and strategy for the loans product. "
+         "      Later on I specialized in Customer Research, working on discovery research, experiment design, and strategy for the loans product. "
          [:a {:href "View my complete resume"}]]
         "<!-- <button type=\"button\" name=\"button\">Read more</button> -->"]]]
      [:div.feature4
       [:div.container-lrg.flex
        [:div.col-5.centervertical
-        [:h3.subheading "\n            Sookasa Inc\n          "]
+        [:h3.subheading "      Sookasa Inc    "]
         [:p.paragraph
-         "\n            As the only full time designer for "
+         "      As the only full time designer for "
          [:a {:href "https://www.sookasa.com/"} "Sookasa"]
          ", I worked on a wide range of products including signup optimization, a full "
          [:a {:href "#"} "UI redesign"]
-         ", and a new threat scanning product\n          "]
+         ", and a new threat scanning product    "]
         "<!-- <button type=\"button\" name=\"button\">Read more</button> -->"]
        [:div.col-1]
        [:div.col-6
         [:div.sidedevices
          [:div.computerwrapper
           [:img.mask-img {:src "img/mac_book.png"}]
-          "<!-- <div class=\"computer\">\n                <div class=\"mask\">\n\n                </div>\n              </div> -->"]]]]]
+          "<!-- <div class=\"computer\">          <div class=\"mask\">    </div>        </div> -->"]]]]]
      [:div.feature2
       [:div.container-lrg
         [:h2 "Additional projects"]
@@ -118,56 +124,56 @@
        [:div.text-center
         [:div.col-12
          [:h4
-          "\n              What people say about me\n            "]]]]
+          "        What people say about me      "]]]]
       [:div.container-lrg
        [:div.tweets.flex
         [:div.col-4
          [:div.testimonial
           [:div.flex.flexvert
            [:div
-            [:b "\n                    James Taylor\n                  "]
+            [:b "              James Taylor            "]
             [:br]
             [:span
-             "\n                    Design Lead at Tyro\n                  "]]]
+             "              Design Lead at Tyro            "]]]
           [:div.testimonial-content
            [:p.paragraph
-            "\n                  Sarah joined Tyro at a pretty crucial time, upon embarking the journey to launch our lending product. She showed great leadership skills, managing the research for many concurrent streams of research around our customers while truly caring about delivering meaningful, helpful solutions.\n                "]]]]
+            "            Sarah joined Tyro at a pretty crucial time, upon embarking the journey to launch our lending product. She showed great leadership skills, managing the research for many concurrent streams of research around our customers while truly caring about delivering meaningful, helpful solutions.          "]]]]
         [:div.col-4
          [:div.testimonial
           [:div.flex.flexvert
            [:div
-            [:b "\n                    Will Jensen\n                  "]
+            [:b "              Will Jensen            "]
             [:br]
             [:span
-             "\n                    CEO at Fetchh\n                  "]]]
+             "              CEO at Fetchh            "]]]
           [:div.testimonial-content
            [:p.paragraph
-            "\n                  After Sarah joined the team, she hit the ground running. Sarah was an amazing asset for us, helping us with not just prototypes and development - but with higher-level thinking about our strategy and overall usability. We surely wouldn't have achieved as much as we wanted without Sarah's help. I would highly recommend Sarah and hope to work together again in the future.\n              "]]]]
+            "            After Sarah joined the team, she hit the ground running. Sarah was an amazing asset for us, helping us with not just prototypes and development - but with higher-level thinking about our strategy and overall usability. We surely wouldn't have achieved as much as we wanted without Sarah's help. I would highly recommend Sarah and hope to work together again in the future.        "]]]]
         [:div.col-4
          [:div.testimonial
           [:div.flex.flexvert
            [:div
-            [:b "\n                    Dan Manian\n                  "]
+            [:b "              Dan Manian            "]
             [:br]
             [:span
-             "\n                    Direct manager at Sookasa\n                  "]]]
+             "              Direct manager at Sookasa            "]]]
           [:div.testimonial-content
            [:p.paragraph
-            "\n                  Sarah joined as Sookasa's first full-time designer and has been instrumental in both elevating the design of our existing products, as well as launching new ones. She owns the entire design process from user research, wireframing, and user testing through HTML/CSS execution, working hand-in-hand with engineering. The breadth of her skills and ability to work both independently and collaboratively make her a huge asset on any team.\n                "]]]]]]]
+            "            Sarah joined as Sookasa's first full-time designer and has been instrumental in both elevating the design of our existing products, as well as launching new ones. She owns the entire design process from user research, wireframing, and user testing through HTML/CSS execution, working hand-in-hand with engineering. The breadth of her skills and ability to work both independently and collaboratively make her a huge asset on any team.          "]]]]]]]
      [:div.footer
       [:div.container-sml
        [:div.col-12.text-center
         [:div
          [:a.nav-link
           {:href "https://www.linkedin.com/in/saraharnegard/"}
-          "\n              LinkedIn\n            "]
+          "        LinkedIn      "]
          [:a.nav-link
           {:href "mailto:sarah.arnegard@gmail.com"}
-          "\n              Email\n            "]
+          "        Email      "]
          [:a.nav-link
           {:href "https://blog.saraharnegard.com/"}
-          "\n              Blog\n            "]]
+          "        Blog      "]]
         [:br]
         [:div
          [:span
-          "\n              © 2017 Sarah Arnegard\n            "]]]]]]])
+          "        © 2017 Sarah Arnegard      "]]]]]]])
