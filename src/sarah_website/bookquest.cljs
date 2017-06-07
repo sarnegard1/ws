55cd0257b1b7e5fc403d82b4b00fb56dda06d0e5
Bryan Maass <bryan.maass@gmail.com>
2017-06-07 11:36:02 -0500

    fixes things

--- src/sarah_website/bookquest.cljs
+++ src/sarah_website/bookquest.cljs
@@ -34,9 +34,9 @@
    [:section.container
     [:h2.section-title "Business Research"]
     [:div.row
-      [:h3 "Reading program competitive landscape"]
-      [:div
-        [:img.img-lrg {:src "img/schol/comp_analysis_chart.png", :alt "Competitive Analysis Diagram"}]]
+     [:h3 "Reading program competitive landscape"]
+     [:div
+      [:img.img-lrg {:src "img/schol/comp_analysis_chart.png", :alt "Competitive Analysis Diagram"}]]
      [:div.col-md-4
       [:h3 "Findings:"]
       [:ul
@@ -48,29 +48,29 @@
      [:h3
       "Current Scholastic outreach efforts to students and parents"]
      [:div.flex
-       [:div.col-5
-        [:div
-         [:img.img-lrg
-          {:src "img/schol/scho2LG.jpg",
-           :alt "Sample Scholastic Book Fair Flyer"}]
-         [:figcaption
-          "Current Scholastic offering focuses around school book fairs that heavily market toys and posters to children"]]]
-       [:div.col-7
-        [:h3 "Method"]
-        [:ul
-         [:li "Researched Scholastic programs through company website"]
-         [:li
-          "Surveyed parents and teachers regarding their thoughts and feelings about Scholastic and their programs\n                              "]]
-        [:h3 "Findings:"]
-        [:ul
-         [:li
-          "Scholastic book fairs market directly to children so they focus on toy tie-ins and music posters, rather than educational material"]
-         [:li
-          "Teachers feel negatively about Scholastic and associate them more with marketing than educational value"]
-         [:li
-          "Parents and teachers surveyed are concerned that students are not excited to buy books but stuff like pens and toys"]
-         [:li
-          "Parents and teachers surveyed feel that Scholastic's current efforts do a poor job in promoting reading"]]]]]]
+      [:div.col-5
+       [:div
+        [:img.img-lrg
+         {:src "img/schol/scho2LG.jpg",
+          :alt "Sample Scholastic Book Fair Flyer"}]
+        [:figcaption
+         "Current Scholastic offering focuses around school book fairs that heavily market toys and posters to children"]]]
+      [:div.col-7
+       [:h3 "Method"]
+       [:ul
+        [:li "Researched Scholastic programs through company website"]
+        [:li
+         "Surveyed parents and teachers regarding their thoughts and feelings about Scholastic and their programs\n                              "]]
+       [:h3 "Findings:"]
+       [:ul
+        [:li
+         "Scholastic book fairs market directly to children so they focus on toy tie-ins and music posters, rather than educational material"]
+        [:li
+         "Teachers feel negatively about Scholastic and associate them more with marketing than educational value"]
+        [:li
+         "Parents and teachers surveyed are concerned that students are not excited to buy books but stuff like pens and toys"]
+        [:li
+         "Parents and teachers surveyed feel that Scholastic's current efforts do a poor job in promoting reading"]]]]]]
    [:section.container
     [:h2.section-title.clear "User Research"]
     [:p "We visited a playground at Yerba Buena Center to conduct user interviews and sent out surveys to parents and teachers in our network"]
@@ -106,10 +106,10 @@
     [:p
      "Using our research, we created empathy maps for our parent user and child user. This helped us develop our personas."]
     [:div.flex
-      [:div.col-6
-       [:img {:src "img/schol/child_persona.png", :alt ""}]]
-      [:div.col-6
-        [:img {:src "img/schol/persona_adult.png", :alt ""}]]]
+     [:div.col-6
+      [:img {:src "img/schol/child_persona.png", :alt ""}]]
+     [:div.col-6
+      [:img {:src "img/schol/persona_adult.png", :alt ""}]]]
     [:div.row
      [:h3 "Problem Statements"]
      [:p
@@ -122,9 +122,9 @@
     [:h3 "SSNiFs"]
     [:p "From here, we used the personas to create SSNiFs (Stakeholder, Situation, Needs and Features charts ). This allowed us to identify possible features that were directly tied to our users' needs. Next, we prioritized the features and decided which ones we would design for our MVP."]
     [:div.flex
-      [:img.img-lrg {:src "img/schol/Parent_SSNiF.png", :alt ""}]]
+     [:img.img-lrg {:src "img/schol/Parent_SSNiF.png", :alt ""}]]
     [:div.flex
-      [:img.img-lrg {:src "img/schol/Jenny_SSNiF.png", :alt ""}]]]
+     [:img.img-lrg {:src "img/schol/Jenny_SSNiF.png", :alt ""}]]]
    [:section.container-lrg
     [:h3.clear "User Flows"]
     [:div.flex
@@ -142,33 +142,33 @@
    [:section.container-lrg
     [:h2 "Wireframes"]
     [:div.flex
-      [:div.col-4
-        [:img {:src "img/schol/reward_1.png", :alt ""}]]
-      [:div.col-4
-       [:img {:src "img/schol/quiz.png", :alt ""}]]
-      [:div.col-4
-       [:img {:src "img/schol/quiz_2.png", :alt ""}]]]]
+     [:div.col-4
+      [:img {:src "img/schol/reward_1.png", :alt ""}]]
+     [:div.col-4
+      [:img {:src "img/schol/quiz.png", :alt ""}]]
+     [:div.col-4
+      [:img {:src "img/schol/quiz_2.png", :alt ""}]]]]
    [:section
     [:h3 "Low-fi Clickable Prototype"]
     [:div.button-container
-      [:a.button-link
-       {:target "_blank", :href "http://invis.io/KC10SCQ8Q"}
-       "Visit complete prototype"]
-      [:small " This will open Invision in a new tab"]]
+     [:a.button-link
+      {:target "_blank", :href "http://invis.io/KC10SCQ8Q"}
+      "Visit complete prototype"]
+     [:small " This will open Invision in a new tab"]]
     [:div.container-lrg
-      [:div.flex
-       [:div.col-6
-        [:img {:src "img/schol/k_quiz_1.png", :alt "..."}]]
-       [:div.col-6
-        [:img {:src "img/schol/k_quiz_2.png", :alt "..."}]]]
-      [:div.flex
-       [:div.col-12
-        [:img {:src "img/schol/k_quiz_3.png", :alt "..."}]]]
-      [:div.flex
-       [:div.col-6
-        [:img {:src "img/schol/k_quiz_4.png", :alt "..."}]]
-       [:div.col-6
-        [:img {:src "img/schol/k_quiz_5.png", :alt "..."}]]]
+     [:div.flex
+      [:div.col-6
+       [:img {:src "img/schol/k_quiz_1.png", :alt "..."}]]
+      [:div.col-6
+       [:img {:src "img/schol/k_quiz_2.png", :alt "..."}]]]
+     [:div.flex
+      [:div.col-12
+       [:img {:src "img/schol/k_quiz_3.png", :alt "..."}]]]
+     [:div.flex
+      [:div.col-6
+       [:img {:src "img/schol/k_quiz_4.png", :alt "..."}]]
+      [:div.col-6
+       [:img {:src "img/schol/k_quiz_5.png", :alt "..."}]]]
      [:div.col-md-4
       [:p
        "There were several goals I wanted to address with the challenges:"]
@@ -186,36 +186,29 @@
          [:strong "Creative Confidence & Engagement:"]
          " open ended prompts to encourage deeper thinking and give importance to the child’s own thoughts and beliefs"]]]]]]])
 
-
-
-(defn section-info
+(def section-info
   [{:section-title "Introduction"
-    :text [:ul
-               [:li
-                [:p [:strong "Educational Value:"]" other reading quizzes were based more on remembering facts. The questions should be about important information from the book rather than trivia"]]
-               [:li
-                [:p [:strong "Fun & Interactivity:"]"create a more engaging experience by using img, videos and other media"]]
-               [:li
-                [:p [:strong "Creative Confidence & Engagement:"]" open ended prompts to encourage deeper thinking and give importance to the child’s own thoughts and beliefs"]]]}
-   {:section-title "Introduction"
-    :text [:ul
-                [:li
-                  [:p [:strong "Educational Value:"]" other reading quizzes were based more on remembering facts. The questions should be about important information from the book rather than trivia"]]
-                [:li
-                  [:p [:strong "Fun & Interactivity:"]"create a more engaging experience by using img, videos and other media"]]
-                [:li
-                  [:p [:strong "Creative Confidence & Engagement:"]" open ended prompts to encourage deeper thinking and give importance to the child’s own thoughts and beliefs"]]]}])
+    :blurbs [{:title "Educational Value"
+            :description "other reading quizzes were based more on remembering facts. The questions should be about important information from the book rather than trivia"}
+           {:title "Fun & Interactivity"
+            :description "create a more engaging experience by using img, videos and other media"}
+           {:title "Creative Confidence & Engagement"
+            :description "open ended prompts to encourage deeper thinking and give importance to the child’s own thoughts and beliefs"}]}])
 
-(defn section [{:keys [section-title text]}]
+(defn section [{:keys [section-title blurbs]}]
   [:section.container-lrg
-    [:h2 section-title]
-    [text]])
+   [:h2 section-title]
+   (into [:ol.blurbs]
+         (for [{:keys [title description] :as b} blurbs]
+           [:li.blurb
+            [:strong title]
+            (str " - " description)]))])
 
 (defn content[]
   [:div
-    [:header.header
-      nav/nav-data]
-    (into [:div]
-          (map section section-info))
-    case-study
-    footer/footer-data])
+   [:header.header
+    nav/nav-data]
+   (into [:div.section]
+         (map section section-info))
+   case-study
+   footer/footer-data])
